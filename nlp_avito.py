ba0febcf7140f36eb4017a37f64a9fc6c2c3fb6d
alsey3tit3nomad <leshavanya2007@gmail.com>
2025-09-22 01:37:50 +0300

    add comments

--- nlp_avito.py
+++ nlp_avito.py
@@ -1,3 +1,6 @@
+# импортируем pandas (нужен для формирования ответа)
+# argparse для передачи названия файла и кодировки
+# transformers для токенизатора
 import pandas as pd
 import argparse
 from transformers import AutoTokenizer
@@ -11,6 +14,7 @@ def run_pipeline(file_path, encoding):
         raw_tokens = f.read().split()
 
     df = []
+    # Надо такие строчки, где в колонке text тоже есть запятые
     for string in raw_tokens[1:]:
         index_comma = string.find(',')
         idx = string[:index_comma]
@@ -19,6 +23,7 @@ def run_pipeline(file_path, encoding):
 
     df = pd.DataFrame(data = df, columns=['id', 'text'])
     tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
+    # Разбиваем текст на токены и считаем позиции пробелов
     def get_space_positions(text):
         text = text.lower()
         tokens = tokenizer.tokenize(text)
@@ -29,10 +34,12 @@ def run_pipeline(file_path, encoding):
             positions.append(current_pos)
         return str(positions)
 
+    # Формируем решение
     df["predicted_positions"] = df["text"].apply(get_space_positions)
     df.to_csv('submission.csv', columns=['id', 'predicted_positions'], index=False)
 
 def main():
+    # Читаем аргументы из командной строки
     parser = argparse.ArgumentParser(description='Токенизация текста')
     parser.add_argument("--file", required=True, help='Путь к файлу .txt')
     parser.add_argument("--encoding", help="Кодировка", required=False)
